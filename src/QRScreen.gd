f49c701959885f7d888faec588d23d95f4433135
Hikari Lua <hikarilua.luz@gmail.com>
2024-10-17 15:55:48 -0300

    add get meeting id logic

--- src/QRScreen.gd
+++ src/QRScreen.gd
@@ -5,13 +5,16 @@ func _ready() -> void:
 	%QRHTTPRequest.request_completed.connect(_on_http_request_request_completed)
 	%OpenLoginButton.toggled.connect(_on_open_login_toggled)
 
-	var domain: String = CfgFile.settings.get("url-api")
-	var route: String = "/meetings/by-date"
-	var params: String = "/%s" % Time.get_date_string_from_system()
+	var url: String = CfgFile.settings["url-api"] + "/meetings"
 
-	var url: String = domain + route + params
+	var json := JSON.new()
+	var body: String = json.stringify(
+		{"date": Time.get_datetime_string_from_system()}
+	)
 
-	var error: Error = %QRHTTPRequest.request(url)
+	var error: Error = %QRHTTPRequest.request(
+		url, [], HTTPClient.METHOD_POST, body
+	)
 	if error != OK:
 		var msg: String = (
 			"Erro ao fazer requisição para %s (%s)" % [url, error_string(error)]
@@ -29,29 +32,30 @@ func _on_http_request_request_completed(
 	headers: PackedStringArray,
 	body: PackedByteArray
 ) -> void:
-	if response_code == 200:
-		var json := JSON.new()
-		json.parse(body.get_string_from_utf8())
+	var json := JSON.new()
+	var error: Error = json.parse(body.get_string_from_utf8())
+	var response_data: Variant = json.get_data()
 
-		var response_data: Variant = json.get_data()
-
-		var meeting_id: int = response_data.get("id")
-
-		var domain: String = CfgFile.settings["url-api"]
-		var route: String = "/meetings"
-		var params: String = "/%d" % meeting_id
-
-		var link: String = domain + route + params
-		print("Link gerado:", link)
-
-		%QRCodeRect.data = link
-		%QRCodeRect.show()
+	var meeting_id: int
+	if response_code == HTTPClient.RESPONSE_CREATED:
+		meeting_id = response_data.get("id")
+	elif response_code == HTTPClient.RESPONSE_CONFLICT:
+		meeting_id = response_data.get("meeting").get("id")
 	else:
 		var err: String = "Erro na requisição, código: %s" % response_code
 		printerr(err)
 
 		%ErrorLabel.text = err
 		%ErrorLabel.show()
+		return
+
+	var domain: String = CfgFile.settings["url-api"]
+	var route: String = "/meetings"
+	var params: String = "/%d" % meeting_id
+
+	var link: String = domain + route + params
+	%QRCodeRect.data = link
+	%QRCodeRect.show()
 
 
 func _on_open_login_toggled(value: bool) -> void:
